fd451982b27bea3906926b3930baa36a2870296a
Seth Gilchrist <irishwulf@gmail.com>
2025-01-12 16:28:12 -0700

    Replace 'fractional' package with 'fraction.js'

--- src/forkify/src/js/views/recipeView.js
+++ src/forkify/src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import Fraction from 'fractional';
+import Fraction from 'fraction.js';
 
 class RecipeView extends View {
   _parentElem = document.querySelector('.recipe');
@@ -137,7 +137,9 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''
+        ingredient.quantity
+          ? new Fraction(ingredient.quantity).toFraction(true)
+          : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ingredient.unit}</span>
